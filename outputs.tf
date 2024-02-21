4f51c49c932e61ceab7f6df4654503649279ddac
akahub <ak@endava.com>
2024-02-21 20:41:13 +0000

    del
    del

--- outputs.tf
+++
@@ -1,7 +0,0 @@
-// Define outputs you need displayed in the terraform console after building the servers.
-// Linux servers just need the Elastic IP.
-
-output "server1_eip" {
-  value = aws_eip.server1.public_ip
-}
-
